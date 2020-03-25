e049253e32ec7e91b8ecfb627880483ff78a410f
Bor Hodošček <dev@bor.space>
2020-03-25 16:15:20 +0900

    lunr ja support (3)
    Signed-off-by: Bor Hodošček <dev@bor.space>

--- static/js/search.js
+++ static/js/search.js
@@ -16,9 +16,9 @@ function initLunr() {
 			pagesIndex = index;
 			// Set up lunrjs by declaring the fields we use
 			// Also provide their boost level for the ranking
-			// lunr.stemmerSupport(lunr);
-			TinySegmenter(lunr);
-			ja(lunr);
+			//lunr.stemmerSupport(lunr);
+			//TinySegmenter(lunr);
+			//ja(lunr);
 			lunrIndex = lunr(function() {
 				this.use(lunr.ja);
 				this.ref("uri");
